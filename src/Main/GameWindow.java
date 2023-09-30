42786a6fa58c518a6ad03cc0a4c5d72b4fe6f1ae
Haikaaal <haikal.athallarik@gmail.com>
2023-09-30 18:45:17 +0700

    Game Window And Test Square in it

---
+++ src/Main/GameWindow.java
@@ -0,0 +1,15 @@
+package Main;
+
+import javax.swing.JFrame;
+
+public class GameWindow{
+    private JFrame jFrame;
+    public GameWindow(GamePanel gamePanel) {
+        JFrame jFrame = new JFrame();
+        jFrame.setSize(400,400);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.add(gamePanel);
+        jFrame.setVisible(true);
+
+    }
+}
